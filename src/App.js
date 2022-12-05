a86473dffca84e3e777f46f50ec86e31c2d00792
hadinata_igna <113482226+hadiigna@users.noreply.github.com>
2022-12-05 19:48:37 +0900

    Add qr scanner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import QrScanner from 'qr-scanner';
+import {useState, useRef, useCallback } from 'react';
+
+function QrCodeCamera() {
+
+  const scannerInstance = useRef();
+
+  const [result, setResult] = useState('Scanning...');
+
+  const videoElemRef = useCallback(async node => {
+    if(node) {
+      scannerInstance.current = new QrScanner(node, 
+        scanResult => setResult(scanResult.data), {
+        onDecodeError: error => {
+          if (error === QrScanner.NO_QR_CODE_FOUND) {
+            setResult("Scanning...");
+          }
+          else {
+            setResult(`ERROR: ${error}`);
+          }
+        },
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+      });
+      QrScanner.listCameras(true);
+      await scannerInstance.current.setCamera('user');
+      await scannerInstance.current.start();
+      console.log("camera started");
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1> {result} </h1>
+      <video ref={videoElemRef}></video>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <QrCodeCamera></QrCodeCamera>
     </div>
   );
 }
